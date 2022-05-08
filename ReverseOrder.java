9bb1c4eb6f6d7ae14025ddd9eee6febe5cde74cc
narsing17 <narsingp14@gmail.com>
2022-05-08 11:07:12 +0530

    Arrays in reverse order

---
+++ ReverseOrder.java
@@ -0,0 +1,16 @@
+public class ReverseOrder {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 2, 3, 4};
+        System.out.println("Array in original Order ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Array in reverse order: ");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
